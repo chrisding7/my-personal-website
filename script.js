92b996df76bfe8ba60891fc0bbe69fa8137f3351
Chris Ding <chrisding7@berkeley.edu>
2022-08-21 12:29:32 -0700

    added mouseover and mouseout events to navbar

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const nav = document.getElementById("nav-list");
 
-nav.addEventListener("mouseover", function hover() {
-    nav.style.color = 'white';
+nav.addEventListener("mouseover", (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "white";
+    }, false);
+
+
+  nav.addEventListener("mouseout", (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "#C39787";
+  }, false);
+
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
