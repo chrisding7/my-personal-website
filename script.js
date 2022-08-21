1bef3e25efef0904fa1a887a35c39d8488ef1e65
Chris Ding <chrisding7@berkeley.edu>
2022-08-21 11:40:42 -0700

    added text content

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "";
-document.querySelector("body").appendChild(h2);
+const nav = document.getElementById("nav-list");
+
+nav.addEventListener("mouseover", function hover() {
+    nav.style.color = 'white';
+});
+
